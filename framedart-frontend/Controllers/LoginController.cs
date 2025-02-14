ea4fe31aafa1fd09f632b31031eb8e950871d152
Vinicius Monteiro <vinicius@iconax.com.br>
2025-02-14 18:10:19 -0300

    feat: iniciando tela de login

---
+++ framedart-frontend/Controllers/LoginController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace framedart_frontend.Controllers {
+    public class LoginController : Controller {
+
+        public ActionResult Index () {
+            return View();
+        }
+
+        public JsonResult ValidarUsuario (string username, string password) {
+            return Json(new { }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
